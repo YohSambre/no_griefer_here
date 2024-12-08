12ef9f1f21edae6de660f52000420b3ad088f455
YohSambre <YohSambre@users.noreply.github.com>
2024-12-08 19:53:34 +0100

    Revert "Optimization"

--- no_griefer_here/lua/autorun/server/ngh_simplyproptool_module_sv.lua
+++ no_griefer_here/lua/autorun/server/ngh_simplyproptool_module_sv.lua
@@ -5,20 +5,16 @@
 --  ╚═══════════════════════════╝
 print("NGH - Simply Prop|Tool Module loaded!")
 -- probably not so useful thanks to the Simply Physgun Module..BUT YOLOOOOOOOOOOOO :dance:
-
-local tClassEntities = {
-    ["prop_physics"] = true,
-    ["gmod_balloon"] = true,
-    ["gmod_wheel"] = true,
-    ["gmod_dynamite"] = true,
-    ["gmod_hoverball"] = true,
-    ["gmod_thruster"] = true
-}
-
 hook.Add("EntityTakeDamage", "NGH_SIMPLY_ANTI_PROPKILL_AND_TOOLKILL_MODULE", function(target, dmginfo)
     if target:IsPlayer() then
         local inflictor = dmginfo:GetInflictor()
-        if IsValid(inflictor) and (tClassEntities[inflictor:GetClass()]) then
+        if IsValid(inflictor) and 
+           (inflictor:GetClass() == "prop_physics" or 
+            inflictor:GetClass() == "gmod_balloon" or 
+            inflictor:GetClass() == "gmod_wheel" or 
+            inflictor:GetClass() == "gmod_dynamite" or 
+            inflictor:GetClass() == "gmod_hoverball" or 
+            inflictor:GetClass() == "gmod_thruster") then
             dmginfo:SetDamage(0) -- '_'
         end
     end
