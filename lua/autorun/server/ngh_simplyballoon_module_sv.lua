b03e5e804514dfcf1e21ee4c1cd3f4d3dcdc49db
YohSambre <YohSambre@users.noreply.github.com>
2025-04-14 22:06:27 +0200

    Update ngh_simplyballoon_module_sv.lua

--- lua/autorun/server/ngh_simplyballoon_module_sv.lua
+++ lua/autorun/server/ngh_simplyballoon_module_sv.lua
@@ -8,8 +8,12 @@
 print("NGH - Simply Balloon Module loaded!")
 -- i couldn't find of anything better..
 local function SoPop(ent)
-    if IsValid(ent) and ent:GetClass() == "gmod_balloon" then
-        ent:SetCollisionGroup(1)
-    end
+timer.Create("SAMBRE_BALLOON_PATCH", 0.1, 0, function()
+    for _, ent in ipairs(ents.FindByClass("gmod_balloon")) do
+        if IsValid(ent) then
+                ent:SetCollisionGroup(1)
+            end
+        end
+    end)
 end
 hook.Add("OnEntityCreated", "NGH_SIMPLY_BALLOON_COLLISION_CATCHER", SoPop)
