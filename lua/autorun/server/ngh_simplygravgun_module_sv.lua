8e0f88dba241984b899e69379e2f57a9f867eff2
YohSambre <YohSambre@users.noreply.github.com>
2025-04-14 22:07:26 +0200

    Update ngh_simplygravgun_module_sv.lua

--- lua/autorun/server/ngh_simplygravgun_module_sv.lua
+++ lua/autorun/server/ngh_simplygravgun_module_sv.lua
@@ -6,22 +6,25 @@
 print("NGH - Simply Gravgun Module loaded!")
 
 hook.Add("GravGunOnPickedUp", "GGP_GRAVGUN_PREVENTION", function(_, ent)
-    if IsValid(ent) and ent:GetClass() == "prop_physics" then
-            return
-        end
+    if not IsValid(ent) then return end
+    if engine.ActiveGamemode() == "darkrp" then
+        if not (ent.CPPIGetOwner and ent:CPPIGetOwner() == ply and ent:GetClass()) then return end
+    end
     ent:SetCollisionGroup(11)
 end)
 
-hook.Add("GravGunOnDropped", "GGP_GRAVGUN_PREVENTION", function(_, ent)
-    if IsValid(ent) and ent:GetClass() == "prop_physics" then
-        timer.Simple(0, function()
-            if IsValid(ent) then
+hook.Add("GravGunOnDropped", "GGP_GRAVGUN_PREVENTION", function(ply, ent)
+    if not IsValid(ent) then return end
+    if engine.ActiveGamemode() == "darkrp" then
+        if not (ent.CPPIGetOwner and ent:CPPIGetOwner() == ply and ent:GetClass()) then return end
+    end
+    timer.Simple(0, function()
+        if not IsValid(ent) then return end
+            ent:SetCollisionGroup(11)
                 timer.Simple(7, function()
                     if IsValid(ent) then
-                        ent:SetCollisionGroup(0)
-                    end
-                end)
+                ent:SetCollisionGroup(0)
             end
         end)
-    end
+    end)
 end)
